93ac604553bbbdde0ef9b8d524013a17db134bf9
Dennis Hedegaard <dennis@dhedegaard.dk>
2016-12-02 18:39:17 +0100

    Forgot to dotnet restore.

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-find . -type d -name "day*" -exec sh -c "echo '*** {} ***' && cd {} && dotnet run" \;
+find . \
+  -type d \
+  -name "day*" \
+  -exec sh -c \
+  "echo '*** {} ***' && cd {} && dotnet restore && dotnet run" \;
