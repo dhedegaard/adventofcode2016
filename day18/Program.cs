a69b07245394dddbe8f4152aa9341b1b8db953e8
Dennis Hedegaard <dennis@dhedegaard.dk>
2016-12-18 14:09:55 +0100

    day18

---
+++ day18/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = ".^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^....";
+
+            var part1 = 0;
+            var part2 = 0;
+            var row = ParseRow(input);
+            part1 += row.Where(e => !e).Count();
+            part2 += row.Where(e => !e).Count();
+            for (var i = 1; i < 400000; i++)
+            {
+                row = IterateRow(row);
+                if (i < 40)
+                    part1 += row.Where(e => !e).Count();
+                part2 += row.Where(e => !e).Count();
+            }
+            Console.WriteLine($"part1: {part1}");
+            Console.WriteLine($"part2: {part2}");
+        }
+
+        public static bool[] IterateRow(bool[] row)
+        {
+            var result = new bool[row.Length];
+            for (var i = 0; i < row.Length; i++)
+            {
+                if ((i == 0 || !row[i - 1]) && (i < row.Length - 1 && row[i + 1]))
+                    result[i] = true;
+                else if ((i > 0 && row[i - 1]) && (i == row.Length - 1 || !row[i + 1]))
+                    result[i] = true;
+            }
+
+            return result;
+        }
+
+        public static bool[] ParseRow(string input)
+        {
+            return input
+                .Select(e => e == '^')
+                .ToArray();
+        }
+    }
+}
